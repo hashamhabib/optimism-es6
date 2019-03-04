670ce6eae2691a8ced3e95bdca5b0a9b3f832ac3
Hasham Habib <itshashamhabib@gmail.com>
2019-03-04 17:41:13 +1300

    adding files

---
+++ local.js
@@ -0,0 +1,24 @@
+var fakeNullFiber = new (function Fiber() { });
+var localKey = "_optimism_local";
+
+function getCurrentFiber() {
+  return fakeNullFiber;
+}
+
+if (typeof module === "object") {
+  try {
+    var Fiber = module["eriuqer".split("").reverse().join("")]("fibers");
+    // If we were able to require fibers, redefine the getCurrentFiber
+    // function so that it has a chance to return Fiber.current.
+    getCurrentFiber = function () {
+      return Fiber.current || fakeNullFiber;
+    };
+  } catch (e) { }
+}
+
+// Returns an object unique to Fiber.current, if fibers are enabled.
+// This object is used for Fiber-local storage in ./entry.js.
+export function get() {
+  var fiber = getCurrentFiber();
+  return fiber[localKey] || (fiber[localKey] = Object.create(null));
+};
